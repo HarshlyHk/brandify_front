4e85e9b1e2db1c3bd5c112d2074ab124adf02b36
Nishant <ntih5565@gmail.com>
2025-06-06 01:49:11 +0530

    Hosting push

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,41 +1,20 @@
-export async function getServerSideProps({ res }) {
-  const baseUrl = "https://dripdrip.in/";
-
-  // Define the pages for your sitemap
-  const pages = [
-    "", // Home page
-    "about", // Example page
-    "contact", // Example page
-    "products", // Example page
+export default async function sitemap() {
+  return [
+    {
+      url: "https://dripdrip.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://dripdrip.in/all-products/oversized-tees",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.dripdrip.in/bottoms",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.dripdrip.in/vests",
+      lastModified: new Date(),
+    },
   ];
-
-  // Generate the XML sitemap
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${pages
-    .map((page) => {
-      return `
-      <url>
-        <loc>${baseUrl}${page}</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-      </url>
-    `;
-    })
-    .join("")}
-</urlset>`;
-
-  // Set the response headers and send the sitemap
-  res.setHeader("Content-Type", "application/xml");
-  res.write(sitemap);
-  res.end();
-
-  return {
-    props: {},
-  };
-}
-
-export default function Sitemap() {
-  return null; // This page is only used to generate the sitemap
 }
