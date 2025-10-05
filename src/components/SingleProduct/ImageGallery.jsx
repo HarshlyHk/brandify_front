b5d9362789727b6cf2d5c09e35be0959423ce875
Nishant <ntih5565@gmail.com>
2025-10-06 00:46:14 +0530

    Order progress time Show

--- src/components/SingleProduct/ImageGallery.jsx
+++ src/components/SingleProduct/ImageGallery.jsx
@@ -264,23 +264,23 @@ const ImageGallery = ({ item, loading, frequentlyBought }) => {
                 {item?.specialSaleDiscount}% SEPCIAL OFF ON THIS ITEM
               </div>
             )}
-            <div className="flex flex-wrap items-center gap-2 md:mt-4">
+            <div className="flex flex-wrap items-center gap-2 mt-4">
               {item?.specialSale == true && (
                 <div className="text-purple-700 hidden md:flex text-[9px] sm:text-[10px] w-fit font-semibold px-2 py-2 mt-4 mb-2 bg-[#f1e2f6] rounded-[5px] items-center gap-2">
                   <BiSolidOffer className=" text-sm" />{" "}
                   {item?.specialSaleDiscount}% SEPCIAL OFF ON THIS ITEM
                 </div>
               )}
-              <div className="text-green-700 text-[8px] sm:text-[10px] w-fit font-semibold px-2 py-2 mt-4 mb-2 bg-[#e2f6e2] rounded-[5px] flex items-center gap-2">
+              <div className="text-green-700 text-[7.5px] sm:text-[10px] w-fit font-semibold px-2 py-2 mb-2 bg-[#e2f6e2] rounded-[5px] flex items-center gap-2">
                 <BiSolidOffer className=" text-sm" /> 10% OFF ON ORDERS ABOVE
                 ₹1199
               </div>
-              <div className="text-green-700 text-[8px] sm:text-[10px] w-fit font-semibold px-2 py-2 mt-4 mb-2 bg-[#e2f6e2] rounded-[5px] flex items-center gap-2">
+              <div className="text-green-700 text-[7.5px] sm:text-[10px] w-fit font-semibold px-2 py-2 mb-2 bg-[#e2f6e2] rounded-[5px] flex items-center gap-2">
                 <BiSolidOffer className=" text-sm" /> 5% OFF ON ORDERS BELOW
                 ₹1199
               </div>
               {item?.isFastDelivery == true && (
-                <div className="text-purple-700 text-[8px] sm:text-[10px] w-fit font-semibold px-2 py-2 mb-2 bg-[#f1e2f6] rounded-[5px] flex items-center gap-2">
+                <div className="text-purple-700 text-[7.5px] sm:text-[10px] w-fit font-semibold px-2 py-2 mb-2 bg-[#f1e2f6] rounded-[5px] flex items-center gap-2">
                   <FaTruck className=" text-sm" /> SAME DAY DISPATCH
                 </div>
               )}
